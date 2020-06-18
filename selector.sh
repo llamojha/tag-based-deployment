d116e3203c25a9a744ba7cf0a7fa89eb9e1819d9
Alvaro Llamojha <alvaro.martinez.llamojha@lego.com>
2020-06-18 21:16:17 +0100

    Create QA workflow in github actions

---
+++ selector.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+PREVIOUS_COMMIT=$(git log -n 1 --skip=1  --pretty=format:"%H")
+LIST=$(git diff ${PREVIOUS_COMMIT}  --name-only)
+PACKAGES=$(ls packages/ )
+ENV="qa"
+
+git log -n 1 --skip=1  --pretty=format:"%H"
+echo "Files changing"
+echo "${LIST}"
+
+for FILE in ${LIST}
+do
+  for PACKAGE in ${PACKAGES}
+  do
+    if [[ ${FILE} =~ "packages/${PACKAGE}"* ]]
+        then
+        echo "Adding TAG ${PACKAGE}-${ENV}"
+        git tag ${PACKAGE}-${ENV} --force
+        git push origin ${PACKAGE}-${ENV} --force
+        PACKAGES=$( echo ${PACKAGES} | sed -e "s/${PACKAGE}//")
+    fi
+  done
+done
