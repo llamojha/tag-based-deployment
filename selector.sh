ecaea01286c8934d5029f59356e36c13f5f81d8c
Alvaro Llamojha <alvaro.martinez.llamojha@lego.com>
2020-06-21 17:12:26 +0100

    Making the output prettier

--- selector.sh
+++ selector.sh
@@ -10,14 +10,14 @@ ENV="qa"
 git log -n 1 --skip=1  --pretty=format:"%H"
 echo "Files changing"
 echo "${LIST}"
-
+echo "##################################################"
 for FILE in ${LIST}
 do
   for PACKAGE in ${PACKAGES}
   do
     if [[ ${FILE} =~ "packages/${PACKAGE}"* ]]
         then
-        echo "Adding TAG ${PACKAGE}-${ENV}"
+        echo "==> Adding TAG ${PACKAGE}-${ENV}"
         git tag ${PACKAGE}-${ENV} --force
         git push origin ${PACKAGE}-${ENV} --force
         PACKAGES=$( echo ${PACKAGES} | sed -e "s/${PACKAGE}//")
